bcc2c3e3d5c1a549da60662f4072d9b439f4f245
SaeedMahmoudSaeed32 <84653270+SaeedMahmoudSaeed32@users.noreply.github.com>
2021-06-11 15:52:10 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 function validation()
 {
     'use strict';
-    var fname=document.getElementById("fname"),
-        lname=document.getElementById("lname"),
-        age=document.getElementById("age"),
+    var age=document.getElementById("age"),
         number=document.getElementById("number"),
-        adresse=document.getElementById("adresse"),
-        select=document.getElementById("select"),
-        email=document.getElementById("email"),
         pass=document.getElementById("pass");
-
-    if(fname.value ==0||fname.value==null)
-    {
-        document.getElementById("div1").innerHTML="please,enter your first name";
-        return false;
-    }
-
-    if(lname.value==0||lname.value==null)
-    {
-        document.getElementById("div2").innerHTML="please,enter your last name";
-        return false;
-    }
-
-    if(age.value==0||age.value==null)
-    {
-        document.getElementById("div3").innerHTML="please,enter your age";
-        return false;
-    }
 
     if(age.value<0)
     {
@@ -40,33 +17,9 @@ function validation()
         return false;
     }
 
-    if(number.value==0||number.value==null)
-    {
-        document.getElementById("div4").innerHTML="please,enter your phone number";
-        return false;
-    }
-
     if(isNaN(number.value))
     {
         document.getElementById("div4").innerHTML="please,enter a valid phone number";
-        return false;
-    }
-
-    if(adresse.value==0||adresse.value==null)
-    {
-        document.getElementById("div5").innerHTML="please,enter your adresse";
-        return false;
-    }
-
-    if(select.value=="")
-    {
-        document.getElementById("div6").innerHTML="please,choose your gender";
-        return false;
-    }
-
-    if(email.value==0||email.value==null)
-    {
-        alert("please,enter your email");
         return false;
     }
 
@@ -84,7 +37,7 @@ function validation()
 
     if((pass.value).charAt(0)!==(pass.value).charAt(0).toUpperCase())
     {
-     document.getElementById("div8").innerHTML="password must start with an upper case character";
+     document.getElementById("div8").innerHTML="password must start with an upper character";
      return false;
     }
 
